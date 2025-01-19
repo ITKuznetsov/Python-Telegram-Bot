2ec25ebfc1bfe580c467d7e2b29dcc763099db00
Nikita Kuznetsov <cskuznetsov@gmail.com>
2025-01-19 17:21:59 +0300

    feat(docker): add containerization refactor(env): replace DATABASE_URL with separate environment variables feat(database): add script to create database from environment variable chore(docker): add .dockerignore file fix(migrations): resolve User model migration bug

--- bot/database/engine.py
+++ bot/database/engine.py
@@ -4,6 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
 
 engine = create_engine(DATABASE_URL)
--- config/config.py
+++ config/config.py
@@ -6,4 +6,9 @@ load_dotenv()
 class BotConfig:
     def __init__(self):
         self.token = os.getenv('BOT_TOKEN')
-        self.database_url = os.getenv('DATABASE_URL')
+        self.postgres_user = os.getenv('POSTGRES_USER')
+        self.postgres_password = os.getenv('POSTGRES_PASSWORD')
+        self.postgres_db = os.getenv('POSTGRES_DB')
+        self.postgres_host = os.getenv('POSTGRES_HOST')
+        self.database_url = (f"postgresql://{self.postgres_user}:{self.postgres_password}"
+                             f"@{self.postgres_host}/{self.postgres_db}")
